141f3076aa20a4d28b26b486c1d6c8218b6aff1a
jlengstorf <jason.lengstorf@copterlabs.com>
2016-10-20 10:52:54 -0700

    chore(build): set up the build process for the new version of the source
    Since this is going to be an ES module, we’re using Rollup.

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+// Rollup plugins
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  entry: 'src/scripts/main.js',
+  dest: 'dist/js/instagram-feed.min.js',
+  format: 'iife',
+  sourceMap: 'inline',
+  plugins: [
+    commonjs(),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    replace({
+      exclude: 'node_modules/**',
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+      IG_CLIENT_ID: JSON.stringify(process.env.IG_CLIENT_ID),
+      IG_REDIRECT_URI: JSON.stringify(process.env.IG_REDIRECT_URI),
+    }),
+    (process.env.NODE_ENV === 'production' && uglify()),
+  ],
+};
