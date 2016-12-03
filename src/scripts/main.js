7410a34c9a1d5b7ebd054fc49330499c19b1be2e
Jason Lengstorf <jason@lengstorf.com>
2016-12-03 17:29:15 -0500

    feat(app): initialized the IG app

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+import loadRecentInstagramPosts from './instagram-feed-reader';
+
 // Create a debugger.
 import debug from 'debug';
 const log = debug('app:main');
@@ -5,5 +7,6 @@ const log = debug('app:main');
 log('Starting the app...');
 
 // Start the app.
+loadRecentInstagramPosts();
 
 log('App started.');
