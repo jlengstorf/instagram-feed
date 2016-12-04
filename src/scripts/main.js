8ed0fe65323d018e43be57f0b98116f75016e35a
Jason Lengstorf <jason@lengstorf.com>
2016-12-04 15:38:03 -0500

    feat(starter): updated the starter to match the article

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,3 @@
-import loadRecentInstagramPosts from './instagram-feed-reader';
-
 // Create a debugger.
 import debug from 'debug';
 const log = debug('app:main');
@@ -7,6 +5,5 @@ const log = debug('app:main');
 log('Starting the app...');
 
 // Start the app.
-loadRecentInstagramPosts({ count: 12 });
 
 log('App started.');
