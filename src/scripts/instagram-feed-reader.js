0800f395dd32e68f26d4fc11cb10e693e21c2cca
Jason Lengstorf <jason@lengstorf.com>
2016-12-04 13:12:48 -0500

    feat(pt3): code as described in pt3

--- src/scripts/instagram-feed-reader.js
+++ src/scripts/instagram-feed-reader.js
@@ -5,16 +5,23 @@ import fetchJSONP from 'fetch-jsonp';
 import bemmit from 'bemmit';
 
 // Only import the functions we need from Ramda.
+import assoc from 'ramda/src/assoc';
 import compose from 'ramda/src/compose';
+import concat from 'ramda/src/concat';
 import curry from 'ramda/src/curry';
 import join from 'ramda/src/join';
 import mapObjIndexed from 'ramda/src/mapObjIndexed';
+import replace from 'ramda/src/replace';
+import tap from 'ramda/src/tap';
 import test from 'ramda/src/test';
 import values from 'ramda/src/values';
 
 // Create a debugger.
 import debug from 'debug';
 const log = debug('instagram-feed-reader');
+
+// Logs whatever is passed, then returns it unchanged.
+const logAndReturn = tap(log);
 
 log('Debugging is enabled!');
 
@@ -38,12 +45,19 @@ const buildLoginLink = args => {
   `;
 };
 
+// Anything with side-effects goes in here to make it really obvious.
 const unsafe = {
   renderStringToDOM: curry((selector, htmlStr) => {
 
     // This is a side-effect.
     document.querySelector(selector).innerHTML = htmlStr;
   }),
+
+  fetchMediaAsJSON: endpoint => {
+    return fetchJSONP(endpoint)
+      .then(data => data.json())
+      .then(logAndReturn);
+  },
 };
 
 // Create a shortcut for rendering into our app’s wrapper element.
@@ -63,12 +77,19 @@ const isLoggedIn = test(/^#access_token=/);
 const getToken = replace('#access_token=', '');
 const addTokenToArgs = assoc('access_token');
 const getQueryStringWithToken = compose(getQueryString, addTokenToArgs);
+const getRecentMediaEndpoint = concat(`${IG_API_SELF_MEDIA_RECENT}?`);
+const buildRequestURI = compose(getRecentMediaEndpoint, getQueryStringWithToken);
 
-export default function initialize() {
+const showPhotos = (args = { count: 16 }) => {
+  const token = getToken(document.location.hash);
+  const endpoint = buildRequestURI(token, args);
+
+  unsafe.fetchMediaAsJSON(endpoint);
+};
+
+export default function initialize(args) {
   if (isLoggedIn(document.location.hash)) {
-
-    // TODO load user media from Instagram
-
+    showPhotos(args);
   } else {
     showLogin();
   }
